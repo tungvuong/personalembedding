37120c10f0661abd0c9dbc86500f801ceb4896ea
root <root@heel.hiit.fi>
2023-03-29 15:07:39 +0300

    gpt

--- gpt_finetune.py
+++ gpt_finetune.py
@@ -219,7 +219,7 @@ def main():
             _daf = []
 
             for row in df['Lyric']:
-                _daf.append(row[:1000])
+                _daf.append(row[:800])
             df = pd.DataFrame({"Lyric":_daf})  
             print(len(df["Lyric"]))
             
